cec8c6b986755ad51f675726c64f57a4efa18c07
AxMari <AlexisMarien@gmail.com>
2016-05-03 15:00:51 -0400

    some house of leave silliness

--- style.js
+++ style.js
@@ -1,22 +1,26 @@
 $(document).ready(function(){
+	var originalText;
 
 
 	$('li').hover(function(){
-		$(this).fadeTo(400, 0.25);
+		$(this).fadeTo(200, 0.15);
 	},
 	function() {
-		$(this).fadeTo(400, 1);
+		$(this).fadeTo(50, 1);
 	});
 	
-	
 
+	$('li').mouseup(function(){
+		$(this).css("background-color", "blue");
+			var originalText = $(this).text();
+			$(this).text(originalText);
+		
 	$('li').mousedown(function(){
 		$(this).css("background-color", "red");
 			$(this).text("clicked");
-	$('li').mouseup(function(){
-		$(this).css("background-color", "blue");
-			$(this).text("unclicked");
-		
+	
+
+
 	});		
 });
 								
